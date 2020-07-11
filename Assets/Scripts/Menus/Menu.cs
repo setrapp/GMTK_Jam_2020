ff0bd94b7df44467810cfa267e3a437bac17b5df
Sam Trapp <trapp.s.e@gmail.com>
2020-07-10 21:18:38 -0600

    Made placeholder Main Menu and a curtain that can transition between levels (no actual visuals)

---
+++ Assets/Scripts/Menus/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+	[SerializeField] bool quitOnEscape = false;
+
+	private void Update()
+	{
+		if (quitOnEscape && Input.GetKeyDown(KeyCode.Escape))
+		{
+			Event_Quit();
+		}
+	}
+
+	public void Event_GotoScene(string scene)
+	{
+		SceneCurtain.ChangeScene(scene);
+	}
+
+	public void Event_Quit()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+	}
+}
---
+++ Assets/Scripts/Menus/SceneCurtain.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneCurtain : MonoBehaviour
+{
+	private static SceneCurtain instance = null;
+	private static SceneCurtain Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				instance = FindObjectOfType<SceneCurtain>();
+			}
+
+			return instance;
+		}
+	}
+
+	[SerializeField] private Animator anim = null;
+	[SerializeField] private string exitSceneParam = "Exit";
+	[SerializeField] private string enterSceneParam = "Enter";
+
+	private string gotoScene = null;
+	private bool prepareEnterAfterExit = false;
+
+	public static void ChangeScene(string scene)
+	{
+		if (Instance == null)
+		{
+			SceneManager.LoadScene(scene);
+			return;
+		}
+
+		Instance.gotoScene = scene;
+		instance.prepareEnterAfterExit = true;
+		instance.PrepareExitScene();
+	}
+
+	private void PrepareExitScene()
+	{
+		if (anim != null)
+		{
+			anim.SetTrigger(exitSceneParam);
+		}
+		else
+		{
+			Event_ExitReady();
+		}
+	}
+
+	public void PrepareEnterScene()
+	{
+		if (anim != null)
+		{
+			anim.SetTrigger(enterSceneParam);
+		}
+		else
+		{
+			Event_EnterReady();
+		}
+	}
+
+	public void Event_ExitReady()
+	{
+		SceneManager.LoadScene(gotoScene);
+		if (prepareEnterAfterExit)
+		{
+			PrepareEnterScene();
+		}
+	}
+
+	public void Event_EnterReady()
+	{
+
+	}
+}
